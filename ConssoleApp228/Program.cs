24a73693dc854b6b0419fe771fbbc44cc4199088
student <student@104-09>
2019-03-02 10:40:48 +0200

    Add main page

--- ConssoleApp228/Program.cs
+++ ConssoleApp228/Program.cs
@@ -1,15 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace ConssoleApp228
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string ConnectionString
         {
+            get
+            {
+                return "698838515:AAGfGg_74AGp4td9tNNIQw_GOuHjfUJCSm8";
+            }
+        }
+        private static TelegramBotClient bot = null;
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Program.bot = new TelegramBotClient(Program.ConnectionString);
+                Console.WriteLine($"{Program.bot.GetMeAsync().Result} 15 574r73d");
+
+                Program.bot.OnMessage += GetMessage;
+                Program.bot.StartReceiving();
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async void GetMessage(object sender, MessageEventArgs e)
+        {
+            try
+            {
+                if (e.Message.Text != null)
+                {
+                    if (e.Message.Text[0] == '/')
+                        await CommandHandler(e);
+                    else
+                        TextHandler(e);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void TextHandler(MessageEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static async Task CommandHandler(MessageEventArgs e)
+        {
+            try
+            {
+                switch (e.Message.Text.ToLower())
+                {
+                    case "/start":
+                        await bot.SendPhotoAsync(e.Message.Chat.Id,
+                            @"http://cdn46.printdirect.ru/cache/product/c9/50/6221865/tov/all/480z480_front_1009_0_0_0_b7c584965630f736252ca84d2103.jpg", 
+                            "PoPKa BoLIT?");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
